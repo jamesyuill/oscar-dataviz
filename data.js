db806d612936eb039c8ae3f3932715818dd16d77
James Yuill <jamesyuill@jamess-mbp.lan>
2025-03-03 20:37:39 +0000

    changes shape of dataset to include details and also winner boolean

--- data.js
+++ data.js
@@ -1,6 +1,159 @@
 const data = [
-  { year: 1928, women: 10, men: 20 },
-  { year: 1929, women: 15, men: 30 },
-  { year: 1930, women: 6, men: 15 },
-  { year: 1931, women: 15, men: 40 },
+  {
+    year: 1928,
+    women: [
+      {
+        name: 'Steff',
+        category: 'best actress',
+        title: 'the lost face',
+        winner: false,
+      },
+      {
+        name: 'brianna',
+        category: 'best makeup',
+        title: 'the lost face',
+        winner: true,
+      },
+    ],
+    men: [
+      {
+        name: 'mark',
+        category: 'best actor',
+        title: 'potatoes',
+        winner: false,
+      },
+      {
+        name: 'steve',
+        category: 'best face',
+        title: 'potatoes',
+        winner: false,
+      },
+      {
+        name: 'jackson',
+        category: 'best newcommer',
+        title: 'potatoes',
+        winner: true,
+      },
+      {
+        name: 'philbery',
+        category: 'over place',
+        title: 'liminy',
+        winner: false,
+      },
+    ],
+  },
+  {
+    year: 1929,
+    women: [
+      {
+        name: 'Steff',
+        category: 'best actress',
+        title: 'the lost face',
+        winner: false,
+      },
+      {
+        name: 'brianna',
+        category: 'best makeup',
+        title: 'the lost face',
+        winner: true,
+      },
+    ],
+    men: [
+      { name: 'mark', category: 'best actor', title: 'potatoes', winner: true },
+      {
+        name: 'steve',
+        category: 'best face',
+        title: 'potatoes',
+        winner: false,
+      },
+      {
+        name: 'jackson',
+        category: 'best newcommer',
+        title: 'potatoes',
+        winner: false,
+      },
+      {
+        name: 'philbery',
+        category: 'over place',
+        title: 'liminy',
+        winner: false,
+      },
+    ],
+  },
+  {
+    year: 1930,
+    women: [
+      {
+        name: 'Steff',
+        category: 'best actress',
+        title: 'the lost face',
+        winner: false,
+      },
+      {
+        name: 'brianna',
+        category: 'best makeup',
+        title: 'the lost face',
+        winner: true,
+      },
+    ],
+    men: [
+      { name: 'mark', category: 'best actor', title: 'potatoes', winner: true },
+      {
+        name: 'steve',
+        category: 'best face',
+        title: 'potatoes',
+        winner: false,
+      },
+      {
+        name: 'jackson',
+        category: 'best newcommer',
+        title: 'potatoes',
+        winner: false,
+      },
+      {
+        name: 'philbery',
+        category: 'over place',
+        title: 'liminy',
+        winner: false,
+      },
+    ],
+  },
+  {
+    year: 1931,
+    women: [
+      {
+        name: 'Steff',
+        category: 'best actress',
+        title: 'the lost face',
+        winner: false,
+      },
+      {
+        name: 'brianna',
+        category: 'best makeup',
+        title: 'the lost face',
+        winner: true,
+      },
+    ],
+    men: [
+      { name: 'mark', category: 'best actor', title: 'potatoes', winner: true },
+      {
+        name: 'steve',
+        category: 'best face',
+        title: 'potatoes',
+        winner: false,
+      },
+      {
+        name: 'jackson',
+        category: 'best newcommer',
+        title: 'potatoes',
+        winner: false,
+      },
+      {
+        name: 'philbery',
+        category: 'over place',
+        title: 'liminy',
+        winner: false,
+      },
+    ],
+  },
 ];
